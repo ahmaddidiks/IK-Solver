d62627f435a4f59e55652b80b2b37935d1ad43b9
ahmaddidiks <adidiksetiyadi@gmail.com>
2020-10-28 18:18:04 +0700

    perthitunagn sore

--- 2 dof.py
+++ 2 dof.py
@@ -1,1 +1,28 @@
-didik = 10
+import math
+import numpy as np
+from uncertainties import ufloat
+x = 10
+y = 0.00
+z = 10.00
+l1 = 13.9
+l2 = 14.6
+#perhitungan Tetha2
+hit = (pow(y,2) + pow(z,2) - pow(l1,2) - pow(l2,2))/2*l1*l2
+cosTetha2 = math.cos(hit)
+sinTetha2 = math.sqrt(1-cosTetha2**2)
+Tetha2 = math.degrees(math.atan2(sinTetha2,cosTetha2))
+#perhitungan Tetha1
+k1 = l1+l2*cosTetha2
+k2 = l2*sinTetha2
+Tetha1 = math.degrees(math.atan2(z,y) - math.atan2(k2,k1))
+
+#perhitungan sudut x
+xDeg = math.atan2(x, math.sqrt(y**2 + z**2))
+print(Tetha1)
+print(Tetha2)
+print(xDeg)
+
+
+
+
+
